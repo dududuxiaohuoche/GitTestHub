ccc44de4942d64ba9eda2efc5e31ad7d46d1a6ff
LiuDa <2724376457@qq.com>
2025-01-20 22:30:53 +0800

    第三次提交

--- Assets/Scripts/scripts_test01.cs
+++ Assets/Scripts/scripts_test01.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class scripts_test01 : MonoBehaviour
 {
     // Start is called before the first frame update
-    private string 字符串 = "Second";
+    private string 字符串 = "Third";
     void Start()
     {
-        
+        字符串 ="乖乖，这是第三次修改了"
     }
 
     // Update is called once per frame
