692e13517f41157fc7ef33b86355ad947dc32b13
LiuDa <2724376457@qq.com>
2025-01-20 21:59:39 +0800

    第一次提交

---
+++ Assets/Scripts/scripts_test01.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripts_test01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string ×Ö·û´® = "First";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
